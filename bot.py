2ae7edfd910598c922f6fe516db9aa5e6d1a9d40
bigfloppa234 <104149053+ceilingtilefan@users.noreply.github.com>
2024-12-15 12:50:54 -0600

    Barely functional attachment/image support

--- bot.py
+++ bot.py
@@ -6,21 +6,26 @@ class MyClient(discord.Client):
         print(f'Logged on as {self.user} using Discord.py version {discord.__version__}')
 
     async def on_message(self, message):
-        if message.flags.forwarded == True:
-            if len(message.message_snapshots[0].content) > 256:
-                await message.channel.send("Forwarded messages must be 256 or fewer in length.", delete_after=5)
-            else:
-                author = message.author
-                authorAvatar = message.author.avatar.url
-                forwardContent = message.message_snapshots[0].content
-                #forwardAttachments = message.message_snapshots[0].attachments
-                forwardEmbed = discord.Embed(
-                    title=f"{forwardContent}",
-                )
-                #forwardEmbed.image(url=forwardAttachments)
-                forwardEmbed.set_footer(text=f"Forwarded by {author}", icon_url=authorAvatar)
+        if message.flags.forwarded == True:   
+            author = message.author
+            authorAvatar = message.author.avatar.url
+            forwardContent = message.message_snapshots[0].content
+            forwardAttachments = message.message_snapshots[0].attachments
+            imageTypes = [".png", ".jpg", ".jpeg", ".webp", ".gif"]
+            embedList = []
+
+            if len(forwardContent) == 0:
+                forwardContent = "Forward contained no text"            
+            forwardEmbed = discord.Embed(description=f"{forwardContent}")
+            forwardEmbed.set_footer(text=f"Forwarded by {author}", icon_url=authorAvatar)
             
-                await message.reply(embed=forwardEmbed, mention_author=False)
+            await message.reply(embed=forwardEmbed, mention_author=False) 
+            #testing = [imageTypes for url in forwardAttachments in imageTypes]
+            if forwardAttachments:
+                for url in forwardAttachments:
+                    newEmbed = discord.Embed(url='https://soggy.cat').set_image(url=str(url))
+                    embedList.append(newEmbed)
+                await message.channel.send(embeds=embedList)
 
 intents = discord.Intents.default()
 intents.message_content = True
