11c132109f1cada5b75ddb08088ba8c5c3c11b9e
bigfloppa234 <104149053+ceilingtilefan@users.noreply.github.com>
2024-12-14 19:57:31 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # forwardrepostbot
- Basic Discord bot to repost Forwarded messages as basic embeds for older clients
+Basic Discord bot to repost forwarded messages as basic embeds for older clients
+
+# Setup
+Place your token in a text file named token.txt
+Install the latest development (2.5.0) of Discord.py and use `python3 bot.py` to start the bot. 
+
+# Features
+Automatically reposts forwarded messages as embeds. Does not work with attachments (yet)
---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}! Discord.py version {discord.__version__}')
+
+    async def on_message(self, message):
+        if message.flags.forwarded == True:
+            print(len(message.content))
+            if len(message.message_snapshots[0].content) > 256:
+                await message.channel.send("Forwarded messages must be 256 or fewer in length.", delete_after=5)
+            else:
+                author = message.author
+                authorAvatar = message.author.avatar.url
+                forwardContent = message.message_snapshots[0].content
+                forwardAttachments = message.message_snapshots[0].attachments
+                forwardEmbed = discord.Embed(
+                    title=f"{forwardContent}",
+                )
+                forwardEmbed.set_footer(text=f"Forwarded by {author}", icon_url=authorAvatar)
+                await message.reply(embed=forwardEmbed, mention_author=False)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+with open('token.txt') as f:
+    token = f.readline()
+client.run(token)
